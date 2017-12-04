2d7fb987b14e08ef60609ed19f988844ba182779
KM Tu <me@kmtu.info>
2017-12-05 01:35:04 +0900

    Accepted

--- prod.cpp
+++ prod.cpp
@@ -1,6 +1,18 @@
 #include "prod.h"
 
 vector<int> Solution::productExceptSelf(vector<int>& nums) {
-    vector<int> ret = {0, 0, 0};
+    int size = nums.size();
+    vector<int> ret(size);
+
+    ret[0] = 1;
+    for (int i = 1; i < size; ++i) {
+        ret[i] = ret[i - 1] * nums[i - 1];
+    }
+
+    int x = 1;
+    for (int i = size - 2; i >=0; --i) {
+        x *= nums[i + 1];
+        ret[i] *= x;
+    }
     return ret;
 }
