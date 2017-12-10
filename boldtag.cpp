81d316c34666c1188df529d7d6a4bcc934181f4d
KM Tu <me@kmtu.info>
2017-12-10 15:54:49 +0900

    Use std::string::compare for matching

--- boldtag.cpp
+++ boldtag.cpp
@@ -1,24 +1,37 @@
 #include "boldtag.h"
 #include <map>
 #include <utility>
+#include <iterator>
 
 using std::string;
 using std::vector;
 using std::map;
 using std::make_pair;
 
-struct Tags {
+string Solution::addBoldTag(string s, vector<string>& dict) {
     typedef map<string::size_type, string::size_type> tag_type;
     tag_type tags;
 
-    void squash() {
-        tag_type::iterator prev = tags.begin();
-        tag_type::iterator it = prev;
-        if (prev != tags.end())
-            ++it;
-        else
-            return;
+    // construct tags map
+    for (string::size_type i = 0; i < s.size(); ++i) {
+        for (const string& str : dict) {
+            int comp = s.compare(i, str.size(), str);
+            if (comp == 0) {
+				auto iend = i + str.size();
+                auto dup = tags.insert(make_pair(i, iend));
+                if (dup.second == false) {
+                    // same index exists, replace with new iend
+                    // which must be larger than the old one
+                    dup.first->second = iend;
+                }
+            }
+        }
+    }
 
+    // resolve overlapped/consecutive tags
+    tag_type::iterator prev = tags.begin();
+    if (prev != tags.end()) {
+        tag_type::iterator it = std::next(prev);
         while(it != tags.end()) {
             if (it->first <= prev->second) {
                 if (it->second > prev->second)
@@ -31,86 +44,13 @@ struct Tags {
             }
         }
     }
-};
-
-class Matcher {
-    const string str;
-    Tags *tags;
-    string::size_type str_start;
-    string::size_type str_idx;
-    string::size_type acc_idx;
-    bool prev_matched;
-    bool all_same_so_far;
-
-    void rewind() {
-        str_idx = 0;
-        prev_matched = false;
-        all_same_so_far = true;
-    }
-
-public:
-    Matcher(string str, Tags *tags) : str(str), tags(tags), acc_idx(0) {
-        rewind();
-    }
-
-    void match(const char& c) {
-        if (str[str_idx] == c) {
-            if (prev_matched) {
-                if (all_same_so_far && (c != str[str_idx-1])) {
-                    all_same_so_far = false;
-                }
-            }
-            else {
-                str_start = acc_idx;
-                prev_matched = true;
-            }
-            ++str_idx;
-            if (str_idx == str.size()){
-                auto dup = tags->tags.insert(make_pair(str_start, acc_idx + 1));
-                if (dup.second == false) {
-                    // same index exists, replace with new acc_idx
-                    // which must be larger than the old one
-                    dup.first->second = acc_idx + 1;
-                }
-                rewind();
-            }
-        }
-        else {
-            if (prev_matched && all_same_so_far && (c == str[str_idx-1])) {
-                ++str_start;
-            }
-            else {
-                rewind();
-            }
-        }
-        ++acc_idx;
-    }
-
-};
-
-string Solution::addBoldTag(string s, vector<string>& dict) {
-    vector<Matcher> matchers;
-    Tags tags;
-
-    for (const auto& str : dict) {
-        matchers.push_back(Matcher(str, &tags));
-    }
-
-    for (const auto& c : s) {
-        for (auto& m : matchers) {
-            m.match(c);
-        }
-    }
-
-    // resolve overlapped/consecutive tags
-    tags.squash();
 
     // construct return string
     string ret;
     string::size_type cur = 0;
     const string btag_open = "<b>";
     const string btag_close = "</b>";
-    for (auto& tag : tags.tags) {
+    for (auto& tag : tags) {
         int len_before_tag = tag.first - cur;
         int len_in_tag = tag.second - tag.first;
         ret.append(s, cur, len_before_tag);
