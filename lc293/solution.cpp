25c8dbdd1d22b8960c4303fe0d7fe4a2b265d705
KM Tu <me@kmtu.info>
2018-01-06 11:49:22 +0900

    Simplified version

--- lc293/solution.cpp
+++ lc293/solution.cpp
@@ -8,12 +8,7 @@ vector<string> Solution::generatePossibleNextMoves(string s) {
     vector<string> ret;
     size_t pos = s.find("++");
     while (pos != string::npos) {
-        string move;
-        if (pos > 0)
-            move += s.substr(0, pos);
-        move += "--";
-        if (pos < s.size() - 2)
-            move += s.substr(pos + 2, s.size() - 1);
+        string move {s.substr(0, pos) + "--" + s.substr(pos + 2)};
         ret.push_back(move);
         pos = s.find("++", pos + 1);
     }
