6c42d9eb331b324c17473719f0f1a929681ec8c8
KM Tu <me@kmtu.info>
2018-01-08 12:26:54 +0900

    Version 3, modify string in place, faster

--- lc294/solution294.cpp
+++ lc294/solution294.cpp
@@ -6,8 +6,11 @@ using std::vector;
 bool Solution294::canWin(string s) {
     size_t pos = s.find("++");
     while (pos != string::npos) {
-        if (!canWin(s.substr(0, pos) + "--" + s.substr(pos + 2)))
+        s[pos] = s[pos+1] = '-';
+        if (!canWin(s)) {
             return true;
+        }
+        s[pos] = s[pos+1] = '+';
         pos = s.find("++", pos + 1);
     }
     return false;
