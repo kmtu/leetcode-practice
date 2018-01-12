bef02433f3816f07ecfdb16d6318b075f0760355
KM Tu <me@kmtu.info>
2018-01-12 12:44:51 +0900

    First stupid version with vector<int> as key

--- lc249/solution249.cpp
+++ lc249/solution249.cpp
@@ -1,10 +1,34 @@
 #include "solution249.h"
+#include <utility>
+#include <unordered_map>
 
 using std::vector;
 using std::string;
+using std::unordered_map;
 
-vector<vector<string>> Solution249::groupStrings(input_type& strings) {
-    return output_type{};
+vector<vector<string>> Solution249::groupStrings(vector<string>& strings) {
+    vector<Key> keys;
+    for (auto str : strings) {
+        Key key;
+        // assert(str.size() >= 1)
+        for (std::size_t i = 0; i < str.size() - 1; ++i) {
+            int k = str[i+1] - str[i];
+            k = k < 0 ? k + num_alphabet : k;
+            key.emplace_back(static_cast<std::size_t>(k));
+        }
+        keys.push_back(std::move(key));
+    }
+
+    unordered_map<Key, vector<string>, HashKey> groups;
+    for (std::size_t i = 0; i < strings.size(); ++i) {
+        Key key = keys[i];
+        groups[key].push_back(strings[i]);
+    }
+    
+    vector<vector<string>> ret;
+    for (auto g : groups)
+        ret.push_back(g.second);
+    return ret;
 }
 
 vector<vector<string>> Solution249::run(input_type strings) {
--- lc249/solution249.h
+++ lc249/solution249.h
@@ -4,6 +4,8 @@
 #include "solution.h"
 #include <vector>
 #include <string>
+#include <functional>
+#include <cstddef>
 
 class Solution249 : public Solution<std::vector<std::string>,
                                     std::vector<std::vector<std::string>>> {
@@ -11,7 +13,18 @@ public:
     output_type run(input_type strings) override;
 
 private:
-    output_type groupStrings(input_type& string);
+    static const int num_alphabet = 26;
+    output_type groupStrings(input_type& strings);
+    using Key = std::vector<std::size_t>;
+    struct HashKey {
+        std::size_t operator()(const Key& key) const {
+            std::size_t ret = std::hash<size_t>()(key.size());
+            for (auto k : key) {
+                ret ^= std::hash<std::size_t>()(k);
+            }
+            return ret;
+        }
+    };
 };
 
 #endif /* SOLUTION249_H */
