77ccba2676cbcbe73c9ce514772d95907c664963
KM Tu <me@kmtu.info>
2018-01-12 19:40:51 +0900

    Use string as keys, much simpler

--- lc249/solution249.cpp
+++ lc249/solution249.cpp
@@ -7,24 +7,17 @@ using std::string;
 using std::unordered_map;
 
 vector<vector<string>> Solution249::groupStrings(vector<string>& strings) {
-    vector<Key> keys;
+    unordered_map<string, vector<string>> groups;
     for (auto str : strings) {
-        Key key;
-        // assert(str.size() >= 1)
+        string key;
         for (std::size_t i = 0; i < str.size() - 1; ++i) {
             int k = str[i+1] - str[i];
-            k = k < 0 ? k + num_alphabet : k;
-            key.emplace_back(static_cast<std::size_t>(k));
+            k = (k < 0 ? k + num_alphabet : k) + 'a';
+            key += k;
         }
-        keys.push_back(std::move(key));
+        groups[key].push_back(std::move(str));
     }
 
-    unordered_map<Key, vector<string>, HashKey> groups;
-    for (std::size_t i = 0; i < strings.size(); ++i) {
-        Key key = keys[i];
-        groups[key].push_back(strings[i]);
-    }
-    
     vector<vector<string>> ret;
     for (auto g : groups)
         ret.push_back(g.second);
--- lc249/solution249.h
+++ lc249/solution249.h
@@ -15,16 +15,6 @@ public:
 private:
     static const int num_alphabet = 26;
     output_type groupStrings(input_type& strings);
-    using Key = std::vector<std::size_t>;
-    struct HashKey {
-        std::size_t operator()(const Key& key) const {
-            std::size_t ret = std::hash<size_t>()(key.size());
-            for (auto k : key) {
-                ret ^= std::hash<std::size_t>()(k);
-            }
-            return ret;
-        }
-    };
 };
 
 #endif /* SOLUTION249_H */
